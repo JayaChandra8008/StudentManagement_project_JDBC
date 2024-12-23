b00e51a67a002628e98e55824d881af7dda22b22
JayaChandra8008 <bagamjaya333@gmail.com>
2024-12-23 17:51:32 +0530

    ServiceImplementation.java

--- ServiceImplementation.java
+++ ServiceImplementation.java
@@ -1,6 +1,7 @@
 package com.databasemanagament.studentmanagment.servicesimplementation;
 
 import com.databasemanagament.studentmanagment.utility.*;
+
 import com.databasemanagament.studentmanagment.dao.StudentDAO;
 import com.databasemanagament.studentmanagment.entity.StudentEntities;
 import com.databasemanagament.studentmanagment.services.StudentService;
@@ -25,22 +26,27 @@ public class ServiceImplementation implements StudentService
         return studentDao.updateStudentData(sAddress, sName, sAge, sId);
     }
     /*-------------------------3-----------------------------------*/
-	@Override
 	public int updateStudentAddress(int stdId, String stdAddress) 
 	{
-		// TODO Auto-generated method stub
 		return studentDao.updateStudentAddress(stdId, stdAddress);
 	}
 //	/*-------------------------4-----------------------------------*/
-//	@Override
-//	public ArrayList<StudentEntities> getStudentList() 
-//	{
-//		// TODO Auto-generated method stub
-//		return studentDao.getStudentList();
-//	}  
+	public ArrayList<StudentEntities> getStudentList() {
+		// TODO Auto-generated method stub
+		return studentDao.getStudentList();
+	}
 	/*-------------------------5-----------------------------------*/
+	@Override
+	public ArrayList<StudentEntities> getStudentListAgeWise(int sAge) 
+	{
+		// TODO Auto-generated method stub
+		return studentDao.getStudentListAgeWise(sAge);
+	}
+	 
+	/*-------------------------6-----------------------------------*/
 	public StudentEntities getStudentDetailsById(int sId) 
 	{
 		return studentDao.getStudentDetailsById(sId);
 	}
+	
 }
