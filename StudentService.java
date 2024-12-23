bf1a9b7ed7679ac61f38ed7e7cd7ced64ea97652
JayaChandra8008 <bagamjaya333@gmail.com>
2024-12-23 17:56:09 +0530

    StudentService.java

--- StudentService.java
+++ StudentService.java
@@ -1,18 +1,22 @@
 package com.databasemanagament.studentmanagment.services;
 
 import com.databasemanagament.studentmanagment.entity.*;
+
 import java.util.*;
 
 public interface StudentService 
 {
+	/*------------Register Student Data-----------*/
     int insertStudent(StudentEntities studentEntityObj);
-
+    /*------------Update Student Data-----------*/
 	int updateStudentData(String sAddress, String sName, int sAge, int sId);
-	
+	/*------------Update Student ID and Address-----------*/
 	int updateStudentAddress(int stdId,String stdAddress);
-
-//	ArrayList<StudentEntities> getStudentList();
-
+	/*------------Retrieve all Student Data-----------*/
+	ArrayList<StudentEntities> getStudentList();
+	/*------------Retrieve all Student Data according to Age-----------*/
+	ArrayList<StudentEntities> getStudentListAgeWise(int sAge);
+	/*------------Retrieve all Student Data according to ID-----------*/
 	StudentEntities getStudentDetailsById(int sId);
 	
 }
